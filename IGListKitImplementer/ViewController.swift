5ef91eba3330766c9eed56d8d2a0bfe5e43bcf27
Javier Loucim <javier.loucim@globant.com>
2018-09-10 17:09:58 -0300

    Initial Commit

---
+++ IGListKitImplementer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IGListKitImplementer
+//
+//  Created by Javier Loucim on 10/09/2018.
+//  Copyright © 2018 Qeeptouch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
